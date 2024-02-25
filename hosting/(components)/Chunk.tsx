c53230a1adba0a507e95c67b2a55f89e4398831d
William Kaiser <wkaisertexas@gmail.com>
2024-02-24 22:36:36 -0500

    updated main page

---
+++ hosting/(components)/Chunk.tsx
@@ -0,0 +1,14 @@
+import Logo from "./Logo";
+
+export default function Chunk() {
+  return (
+    <div className="flex gap-5 max-md:flex-col max-md:gap-0 max-md:">
+      <Logo />
+      <div className="flex flex-col ml-5 w-[56%] max-md:ml-0 max-md:w-full">
+        <div className="self-stretch my-auto text-7xl font-bold tracking-tighter text-black whitespace-nowrap max-md:mt-10 max-md:text-4xl">
+          Trip Dump
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ hosting/(components)/Logo.tsx
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <div className="flex flex-col w-[44%] max-md:ml-0 max-md:w-full">
+      <img
+        loading="lazy"
+        src="https://cdn.builder.io/api/v1/image/assets/TEMP/af29a58ff99b5911ed96ff50c620ab10bea4e4c208cb1de40340e82450c1c2c1?"
+        className="w-full aspect-square fill-white fill-opacity-0"
+      />
+    </div>
+  );
+}
